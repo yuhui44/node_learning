0a54f6d4fa8bf1b894f068c1ac7c6dbe41a1eca1
yuhui44 <yuhui44@live.cn>
2017-09-26 11:30:45 +0800

    9.26am

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,8 @@
 var querystring = require("querystring");
+var fs = require('fs');
+var formidable = require('formidable');
 
-function start(response, postData){
+function start(response){
   console.log("Request handler 'start' was called.");
   // var content = "empty";
 
@@ -9,10 +11,14 @@ function start(response, postData){
     '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="sumit" value="Submit text" />'+
-    '</form>'+
+    '<form action="upload" enctype="multipart/form-data" method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'
+    // '<form action="/upload" method="post">'+
+    // '<textarea name="text" rows="20" cols="60"></textarea>'+
+    // '<input type="sumit" value="Submit text" />'+
+    // '</form>'+
     '</body>'+
     '</html>';
 
@@ -28,13 +34,40 @@ function start(response, postData){
   // return content;
 }
 
-function upload(response, postData){
+function upload(response, request){
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200,{"Content-Type":"text/plain"});
-  response.write("You've sent"+ querystring.parse(postData).text);
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files){
+    console.log("parsing done");
+    fs.renameSync(files.upload.path,"/tmp/test.png");
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  })
+  // response.writeHead(200,{"Content-Type":"text/plain"});
+  // response.write("You've sent"+ querystring.parse(postData).text);
+  // response.end();
   // return "Hello Upload";
+}
+
+function show(response){
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png","binary",function(error, file){
+    if(error){
+      response.writeHead(500,{"Content-Type":"text/plain"});
+      response.write(error +"\n");
+      response.end();
+    }else{
+      response.writeHead(200,{"Content-Type":"image/png"});
+      response.write(file,"binary");
+      response.end();
+    }
+  })
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js
+++ server.js
@@ -3,20 +3,25 @@ var url = require("url");
 
 function start(route, handle){
   function onRequest(request, response){
-    var postData = "";
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for "+pathname+" received.");
+    console.log("Request for "+ pathname +"received.");
+    route(handle, pathname, response, request);
+  }
+  // function onRequest(request, response){
+  //   var postData = "";
+  //   var pathname = url.parse(request.url).pathname;
+  //   console.log("Request for "+pathname+" received.");
     
-    request.setEncoding("utf8");
+  //   request.setEncoding("utf8");
 
-    request.addListener("data",function(postDataChunk){
-      postData += postDataChunk;
-      console.log("Received POST data chunk '"+postDataChunk+"'.");
-    });
+  //   request.addListener("data",function(postDataChunk){
+  //     postData += postDataChunk;
+  //     console.log("Received POST data chunk '"+postDataChunk+"'.");
+  //   });
 
-    request.addListener("end",function(){
-      route(handle, pathname, response, postData);
-    });
+  //   request.addListener("end",function(){
+  //     route(handle, pathname, response, postData);
+  //   });
 
     // route(handle, pathname, response);
 
@@ -25,7 +30,7 @@ function start(route, handle){
     // response.write(content);
     // // response.write("Hello World");
     // response.end();
-  }
+  // }
   
   http.createServer(onRequest).listen(8888);
   
